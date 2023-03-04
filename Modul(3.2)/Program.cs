35e8c64209c9187c91ba8f13b564f38a2fd4bd7c
SergejBoyko <sergej13boyko@mail.ru>
2023-03-04 08:59:00 +0700

    Добавьте файлы проекта.

---
+++ Modul(3.2)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; // Подключенное пространство имен
+
+namespace FirstApplication.ConsoleApp // Объявление пространства имен для данного кода
+{
+    class Program // Объявление класса Program
+    {
+        static void Main(string[] args) // Объявление метода Main
+        {
+
+            string MyName;
+            MyName = "Сергей";
+            Console.WriteLine (MyName);
+            Console.ReadKey();
+        }   
+    }
+}
