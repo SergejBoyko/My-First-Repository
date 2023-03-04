c7b1e10ca4e6ed13d3713ec42bb902c1c9a67889
SergejBoyko <sergej13boyko@mail.ru>
2023-03-04 10:11:27 +0700

    Change 2

--- Modul(3.2)/Program.cs
+++ Modul(3.2)/Program.cs
@@ -11,13 +11,40 @@ namespace FirstApplication.ConsoleApp // Объявление пространс
             //MyName = "Сергей";
             //Console.WriteLine(MyName);
             //Задание 3.2.4
-            string MyName = "Сергей";
-            Console.WriteLine(MyName);
-            Console.WriteLine("\tПривет,мир"+"\n\tМне 25 лет"+"\n\tMy name is \n  Сергей");
+            //string MyName = "Сергей";
+            //Console.WriteLine(MyName);
+            //Console.WriteLine("\tПривет,мир"+"\n\tМне 25 лет"+"\n\tMy name is \n  Сергей");
             //Задание 3.2.8
-            Console.WriteLine("\u0040");
-            Console.WriteLine(" \x23");
-            Console.ReadKey();
-        }   
+            //Console.WriteLine("\u0040");
+            //Console.WriteLine(" \x23");
+            //Console.ReadKey();
+            //Задание 3.3.2
+            //string MyName = "Sergej";
+            //byte MyAge = 25;
+            //bool HaveIApet = false;
+            //double MyShoeSize = 42;
+
+            //Console.WriteLine("My name is " + MyName);
+            //Console.WriteLine("MyAge " + MyAge);
+            //Console.WriteLine("Do I have a pet? " + HaveIApet);
+            //Console.WriteLine("My shoe size is " + MyShoeSize);
+            //Задание 3.4.3
+            //enum DaysOfWeek : byte
+            //{
+            //    Monday = 1,
+            //    Tuesday,
+            //    Wednesday,
+            //    Thursday,
+            //    Friday,
+            //    Saturday
+            //    Sunday
+            //}
+            //enum Semaphore : int
+            //{
+            //    Red = 100,
+            //    Yellow = 200,
+            //    Green = 300,
+            //}
+    }   
     }
 }
