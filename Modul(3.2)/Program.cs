ccd4067eddba31687f76bbe420cdb4d6eaad3366
SergejBoyko <sergej13boyko@mail.ru>
2023-03-04 09:39:15 +0700

    change

--- Modul(3.2)/Program.cs
+++ Modul(3.2)/Program.cs
@@ -6,10 +6,17 @@ namespace FirstApplication.ConsoleApp // Объявление пространс
     {
         static void Main(string[] args) // Объявление метода Main
         {
-
-            string MyName;
-            MyName = "Сергей";
-            Console.WriteLine (MyName);
+            //Задание 3.2.4
+            //string MyName;
+            //MyName = "Сергей";
+            //Console.WriteLine(MyName);
+            //Задание 3.2.4
+            string MyName = "Сергей";
+            Console.WriteLine(MyName);
+            Console.WriteLine("\tПривет,мир"+"\n\tМне 25 лет"+"\n\tMy name is \n  Сергей");
+            //Задание 3.2.8
+            Console.WriteLine("\u0040");
+            Console.WriteLine(" \x23");
             Console.ReadKey();
         }   
     }
